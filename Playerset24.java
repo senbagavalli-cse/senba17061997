be9f5f5d502ca89090459c960da0755452b58d89
senbagavalli-cse <senbaramachandran@gmail.com>
2017-07-22 15:02:21 +0530

    Add files via upload

---
+++ Playerset24.java
@@ -0,0 +1,25 @@
+package guviprgrms;
+
+import java.util.Scanner;
+
+public class Playerset24 {
+
+	
+	public static void main(String[] args) {
+		{
+			Scanner s=new Scanner(System.in);
+			String s1=s.nextLine();
+			StringBuffer st=new StringBuffer(s1);
+			st.reverse();
+			System.out.println(st);
+			for(int i=0;i<st.length();i++)
+			{
+			if(st.charAt(i)=='a' || st.charAt(i)=='e' || st.charAt(i)=='i' || st.charAt(i)=='o' ||st.charAt(i)=='u' ||st.charAt(i)=='A' || st.charAt(i)=='E' || st.charAt(i)=='I' || st.charAt(i)=='O' ||st.charAt(i)=='U')
+			{
+			st.deleteCharAt(i);
+			}
+			System.out.print(st.charAt(i));
+			}
+	}
+	}
+}
