0e140ec8de2c04f3fb664b7b35df8bcce712976a
senbagavalli-cse <senbaramachandran@gmail.com>
2017-07-22 10:30:07 +0530

    Add files via upload

---
+++ Sumforloop.java
@@ -0,0 +1,18 @@
+package guviprgrms;
+
+import java.util.Scanner;
+
+public class Sumforloop {
+public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i,sum=0;
+		for (i=1;i<=n;i++){
+			sum=sum+i;
+			
+		}
+		System.out.println(sum);
+
+	}
+
+}
