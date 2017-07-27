29aa7c47b3b08725546e170440b1c663b0be665a
senbagavalli-cse <senbaramachandran@gmail.com>
2017-07-27 15:47:24 +0530

    Add files via upload

---
+++ Playerset14.java
@@ -0,0 +1,13 @@
+package guviprgrms;
+
+import java.util.Scanner;
+
+public class Playerset14 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int n=Integer.parseInt(s);
+System.out.println("Integer"+n);
+	}
+}
